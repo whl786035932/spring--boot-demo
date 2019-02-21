a953fd4586474271f84137c1fbdb92b2921a6580
DESKTOP-MTMF5NF\whl <wanghaili@videoworks.cn>
2019-02-21 15:53:38 +0800

    commit

--- springBoot/src/main/java/com/example/springBoot/config/WebsecurityConfigureAdapter.java
+++ springBoot/src/main/java/com/example/springBoot/config/WebsecurityConfigureAdapter.java
@@ -2,19 +2,21 @@ package com.example.springBoot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * 在生产环境中使用HTTPS
  * @author whl
  *
  */
-@Configuration
+@EnableWebSecurity
 public class WebsecurityConfigureAdapter  extends  WebSecurityConfigurerAdapter {
 
 
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		 http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 }
